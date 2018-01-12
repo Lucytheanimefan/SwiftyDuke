b0c5c21e2018ac44be4d5ee4240e54314adc6cfd
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-12 09:46:35 -0500

    refactoring out categorypickerviewcontroller

--- DemoApp/ViewControllers/CurriculumViewController.swift
+++ DemoApp/ViewControllers/CurriculumViewController.swift
@@ -8,24 +8,25 @@
 
 import UIKit
 import SwiftyDuke
-class CurriculumViewController: UIViewController {
 
-    @IBOutlet weak var subjectField: UITextField!
-    @IBOutlet var fieldDropDown: UIPickerView!
-    @IBOutlet weak var tableView: UITableView!
+class CurriculumViewController: CategoryPickerViewController {
+
+//    @IBOutlet weak var subjectField: UITextField!
+//    @IBOutlet var fieldDropDown: UIPickerView!
+//    @IBOutlet weak var tableView: UITableView!
     
-    var selectedSubjectField:String! = ""
     var selectedCourseTitle:String! = ""
     var selectedCourseID:String! = ""
     var selectedCourseOfferNumber:String! = ""
-    var subjectFields = [String]()
-    var filteredSubjectFields = [String]()
+    //var subjectFields = [String]()
+    //var filteredSubjectFields = [String]()
     
-    var searchActive:Bool! = false
+    //var searchActive:Bool! = false
     
     var courses = [[String:Any]]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
         loadCurriculumFields()
         
     }
@@ -36,13 +37,13 @@ class CurriculumViewController: UIViewController {
     }
 
     @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
-        self.subjectField.resignFirstResponder()
+        //self.subjectField.resignFirstResponder()
     }
     
     
     
     func loadCurriculum(){
-        SDCurriculum.shared.getCoursesForSubject(subject: self.selectedSubjectField, accessToken: SDConstants.Values.testToken) { (classes) in
+        SDCurriculum.shared.getCoursesForSubject(subject: self.selectedField, accessToken: SDConstants.Values.testToken) { (classes) in
             self.courses = classes
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -116,60 +117,27 @@ extension CurriculumViewController: UITableViewDelegate, UITableViewDataSource{
     }
 }
 
-extension CurriculumViewController: UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate{
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        let tmpFields = searchActive ? self.filteredSubjectFields : self.subjectFields
-        return tmpFields.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let tmpFields = searchActive ? self.filteredSubjectFields : self.subjectFields
-        
-        guard tmpFields.count >= row else {
-            return nil
+extension CurriculumViewController: CategoryPickerDelegate{
+    func loadFields() {
+        SDCurriculum.shared.curriculumValues(field: SDConstants.CurriculumField.subject, accessToken: SDConstants.Values.testToken) { (fields) in
+            self.subjectFields = fields.map({ (json) -> String in
+                return "\(json["code"] as! String) - \(json["desc"] as! String)"
+            })
+            DispatchQueue.main.async {
+                self.fieldDropDown.reloadAllComponents()
+            }
         }
-        return tmpFields[row]
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let tmpFields = searchActive ? self.filteredSubjectFields : self.subjectFields
-        self.selectedSubjectField = tmpFields[row]
-        self.subjectField.text = self.selectedSubjectField
-        loadCurriculum()
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        searchActive = true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        searchActive = false
-    }
-  
-    
-    @IBAction func subjectFieldChanged(_ sender: UITextField) {
-        let filterTerm = sender.text
-        
-        guard filterTerm != nil && (filterTerm!.count > 0) else {
-            searchActive = false
-            self.fieldDropDown.reloadAllComponents()
-            return
+    func loadResponse() {
+        SDCurriculum.shared.getCoursesForSubject(subject: self.selectedField, accessToken: SDConstants.Values.testToken) { (classes) in
+            self.courses = classes
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
-        
-        self.filteredSubjectFields = self.subjectFields.filter({ (field) -> Bool in
-
-            let include = SDParser.textInString(filterTerm: filterTerm!, text: field as NSString)
-
-            return include
-            
-        })
-        
-        self.fieldDropDown.reloadAllComponents()
     }
     
-
 }
+
+

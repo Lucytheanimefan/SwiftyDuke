8fbf17da7be3edc48162afe8b173a08ac02e4916
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-04-10 22:09:18 -0400

    SDPrinter and SDAuthenticator updates as well as unit tests

--- SwiftyDuke/SDAuthenticator.swift
+++ SwiftyDuke/SDAuthenticator.swift
@@ -15,15 +15,16 @@ public class SDAuthenticator: NSObject {
     
     static let shared = SDAuthenticator()
     
-    public func authenticate(clientID:String, redirectURI:String, scope:String, completion:@escaping ([String:Any]) -> Void){
-        let endpoint = "authorize.php?response_type=code&client_id=\(clientID)&scope=\(scope)&redirect_uri=\(redirectURI)"
+    //https://oauth.oit.duke.edu/oauth/authorize.php?client_id=kungfoods&client_secret=imnotputtingoursecretheredoyouthinkimdumb&redirect_uri=http%3A%2F%2Flocalhost%3A1717&response_type=token&state=1129&scope=basic
+    public func authenticate(clientID:String, clientSecret:String, redirectURI:String, completion:@escaping ([String:Any]) -> Void){
+        let endpoint = "authorize.php?response_type=token&client_id=\(clientID)&client_secret=\(clientSecret)&scope=basic&redirect_uri=\(redirectURI)"
         
         requester.makeHTTPRequest(method: "GET", endpoint: endpoint, headers: nil, body: nil, error: { (message) in
             os_log("%@: Error: %@", self.description, message)
         }, completion: { (response) in
-            os_log("%@: Response: %@", self.description, response as! [String:Any])
+            print("Response: \(response)")
             completion(response as! [String:Any])
         })
     }
-
+    
 }
---
+++ SwiftyDuke/SDPrinter.swift
@@ -0,0 +1,35 @@
+//
+//  SDPrinter.swift
+//  SwiftyDuke
+//
+//  Created by Lucy Zhang on 4/10/18.
+//  Copyright © 2018 Lucy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SDPrinter: NSObject {
+    
+    static let shared = SDPrinter()
+    
+    public enum PrinterStatus:String {
+        case idle = "idle", printing = "printing", offline = "offline", all = "all"
+    }
+    
+    public func get3dPrinters(error: @escaping (String) -> Void, printerStatus: PrinterStatus = .all, completion:@escaping ([[String:Any]]) -> Void){
+        SDRequester.colab.makeHTTPRequest(method: "GET", endpoint: "3dprinters/v1/printers", headers: ["x-api-key": "api-docs"], body: nil, error: {(message) in
+            error(message)
+        }, completion: { (response) in
+            guard let json = response as? [[String:Any]] else {
+                error("Data for 3d printers")
+                return
+            }
+            let jsonReturn = json.filter({ (keyValuePair) -> Bool in
+                
+                return (printerStatus.rawValue == "all") || ((keyValuePair["state"] as! String) == printerStatus.rawValue)
+            })
+            completion(jsonReturn)
+        })
+    }
+    
+}
